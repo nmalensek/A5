f86f9594eebf2730172e68ae1a4f1b3fbe444c4c
nmalensek <nicholas@Nicholass-MacBook-Air.local>
2016-11-17 16:30:26 -0700

    created administrator GUI frames/panels

---
+++ src/cs414/a5/nmalensk/gui/AdministratorGUI.java
@@ -0,0 +1,162 @@
+package cs414.a5.nmalensk.gui;
+
+import cs414.a5.nmalensk.common.TransactionLogInterface;
+
+import java.awt.EventQueue;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+public class AdministratorGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtStartdate;
+	private JTextField txtStarttime;
+	private JTextField txtEnddate;
+	private JTextField txtEndTime;
+	private JLabel lblEndDateformat;
+	private JLabel lblStartTime;
+	private JLabel lblEndTime;
+	private JButton btnOk;
+	private JLabel lblEnterTheDate;
+	private DialogBoxes error;
+	private JLayeredPane mainPane;
+	private JPanel initialPanel;
+	private TransactionLogInterface log;
+	private List gateList;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdministratorGUI frame = new AdministratorGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AdministratorGUI(TransactionLogInterface log, List gateList) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		mainPane = new JLayeredPane();
+		mainPane.setBounds(6, 6, 438, 266);
+		contentPane.add(mainPane);
+		
+		initialPanel = new JPanel();
+		initialPanel.setBounds(0, 0, 438, 266);
+		mainPane.add(initialPanel);
+		initialPanel.setLayout(null);
+		
+		txtStartdate = new JTextField();
+		txtStartdate.setBounds(46, 84, 130, 26);
+		initialPanel.add(txtStartdate);
+		txtStartdate.setColumns(10);
+		
+		txtStarttime = new JTextField();
+		txtStarttime.setBounds(237, 84, 130, 26);
+		initialPanel.add(txtStarttime);
+		txtStarttime.setColumns(10);
+		
+		txtEnddate = new JTextField();
+		txtEnddate.setBounds(46, 168, 130, 26);
+		initialPanel.add(txtEnddate);
+		txtEnddate.setColumns(10);
+		
+		txtEndTime = new JTextField();
+		txtEndTime.setBounds(237, 168, 130, 26);
+		initialPanel.add(txtEndTime);
+		txtEndTime.setColumns(10);
+		
+		JLabel lblStartDateformat = new JLabel("Start date (YYYY-MM-dd):");
+		lblStartDateformat.setBounds(14, 56, 162, 16);
+		initialPanel.add(lblStartDateformat);
+		
+		lblEndDateformat = new JLabel("End date (YYYY-MM-dd):");
+		lblEndDateformat.setBounds(14, 140, 156, 16);
+		initialPanel.add(lblEndDateformat);
+		
+		lblStartTime = new JLabel("Start time (HH:mm):");
+		lblStartTime.setBounds(237, 56, 125, 16);
+		initialPanel.add(lblStartTime);
+		
+		lblEndTime = new JLabel("End time (HH:mm):");
+		lblEndTime.setBounds(237, 140, 119, 16);
+		initialPanel.add(lblEndTime);
+		
+		btnOk = new JButton("Ok");
+		btnOk.setBounds(175, 231, 75, 29);
+		initialPanel.add(btnOk);
+		
+		lblEnterTheDate = new JLabel("Enter the date range to analyze:");
+		lblEnterTheDate.setBounds(117, 6, 198, 16);
+		initialPanel.add(lblEnterTheDate);
+		
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					LocalDateTime start = parseDateTime(getDateTime(txtStartdate, txtStarttime));
+					LocalDateTime end = parseDateTime(getDateTime(txtEnddate, txtEndTime));
+					createReportGUI(start, end);
+				} catch (DateTimeParseException dtpe) {
+					error.alertDialog("Invalid input, please check your formatting!",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+
+		error = new DialogBoxes();
+		this.log = log;
+		this.gateList = gateList;
+	}
+
+	private String getDateTime(JTextField dateField, JTextField timeField) {
+		setDateTime(dateField, timeField);
+		String formattedDateTime = "";
+		formattedDateTime += dateField.getText();
+		formattedDateTime += "T";
+		formattedDateTime += timeField.getText();
+		return formattedDateTime;
+	}
+
+	private void setDateTime(JTextField dateField, JTextField timeField) {
+		dateField.setText(dateField.getText());
+		timeField.setText(timeField.getText());
+	}
+
+	private LocalDateTime parseDateTime(String dateTime) throws DateTimeParseException {
+		return LocalDateTime.parse(dateTime);
+	}
+
+	private void createReportGUI(LocalDateTime start, LocalDateTime end) {
+		ReportGenerationGUI reportGenerationGUI = new ReportGenerationGUI();
+		reportGenerationGUI.showReportGenerationGUI(this, start, end);
+		hideInitialPane();
+	}
+
+	public void setLayer(JPanel panel) {mainPane.setLayer(panel, 1);}
+
+	public void showInitialPane() { initialPanel.setVisible(true); }
+	public void hideInitialPane() { initialPanel.setVisible(false); }
+
+	public void addPanel(JPanel panel) { mainPane.add(panel); }
+	public void removePanel(JPanel panel) { mainPane.remove(panel); }
+}
---
+++ src/cs414/a5/nmalensk/gui/ReportGenerationGUI.java
@@ -0,0 +1,121 @@
+package cs414.a5.nmalensk.gui;
+
+import cs414.a5.nmalensk.common.GateReportGeneratorInterface;
+import cs414.a5.nmalensk.common.ReportGeneratorInterface;
+import cs414.a5.nmalensk.common.TransactionLogInterface;
+import cs414.a5.nmalensk.server.GateReportGeneratorImplementation;
+import cs414.a5.nmalensk.server.ReportGeneratorImplementation;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ReportGenerationGUI {
+
+    private JPanel reportPanel;
+    private JLabel lblSelectReportType;
+    private JLabel lblStart;
+    private JLabel lblEnd;
+    private JButton btnHourlyOccupancy;
+    private JButton btnSales;
+    private JButton btnGateEntryexit;
+    private JButton btnNormallostTickets;
+    private JButton btnBack;
+    private TransactionLogInterface log;
+    private List gateList;
+    private DialogBoxes dialog;
+    private ReportGeneratorInterface rGI;
+    private GateReportGeneratorInterface gateReports;
+
+    public ReportGenerationGUI(TransactionLogInterface log, List gateList) throws RemoteException {
+        this.log = log;
+        this.gateList = gateList;
+        dialog = new DialogBoxes();
+        rGI = new ReportGeneratorImplementation();
+        gateReports = new GateReportGeneratorImplementation(gateList);
+    }
+
+    /**
+     * @wbp.parser.entryPoint
+     */
+    public void showReportGenerationGUI(AdministratorGUI adminGUI, LocalDateTime start,
+                                        LocalDateTime end) {
+
+        reportPanel = new JPanel();
+        reportPanel.setBounds(0, 0, 438, 266);
+        reportPanel.setLayout(null);
+        reportPanel.setVisible(true);
+
+        lblSelectReportType = new JLabel("Select report type to generate for range");
+        lblSelectReportType.setBounds(88, 6, 259, 16);
+        reportPanel.add(lblSelectReportType);
+
+        lblStart = new JLabel(String.valueOf(start) + "     -");
+        lblStart.setBounds(61, 34, 162, 16);
+        reportPanel.add(lblStart);
+
+        lblEnd = new JLabel(String.valueOf(end));
+        lblEnd.setBounds(235, 34, 134, 16);
+        reportPanel.add(lblEnd);
+
+        btnHourlyOccupancy = new JButton("Hourly occupancy");
+        btnHourlyOccupancy.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    generateOccupancyReport(start, end);
+                } catch (RemoteException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        btnHourlyOccupancy.setBounds(33, 99, 157, 29);
+        reportPanel.add(btnHourlyOccupancy);
+
+        btnSales = new JButton("Sales");
+        btnSales.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        btnSales.setBounds(235, 99, 157, 29);
+        reportPanel.add(btnSales);
+
+        btnGateEntryexit = new JButton("Gate entries/exits");
+        btnGateEntryexit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        btnGateEntryexit.setBounds(34, 168, 157, 29);
+        reportPanel.add(btnGateEntryexit);
+
+        btnNormallostTickets = new JButton("Normal/lost tickets");
+        btnNormallostTickets.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        btnNormallostTickets.setBounds(235, 168, 157, 29);
+        reportPanel.add(btnNormallostTickets);
+
+        btnBack = new JButton("Back");
+        btnBack.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                reportPanel.setVisible(false);
+                adminGUI.removePanel(reportPanel);
+                adminGUI.showInitialPane();
+            }
+        });
+        btnBack.setBounds(157, 219, 117, 29);
+        reportPanel.add(btnBack);
+
+        adminGUI.setLayer(reportPanel);
+        adminGUI.getContentPane().add(reportPanel);
+    }
+
+    private void generateOccupancyReport(LocalDateTime start, LocalDateTime end) throws RemoteException {
+        dialog.alertDialog(rGI.printHourlyOccupancyData(log, start, end), JOptionPane.INFORMATION_MESSAGE);
+    }
+
+
+}
